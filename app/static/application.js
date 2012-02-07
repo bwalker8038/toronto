0388b559435de9ab463bd96a1d0748963cb42903
Brad Walker <bwalker8038@gmail.com>
2012-02-07 01:57:27 -0500

    added README.md

--- app/static/application.js
+++ app/static/application.js
@@ -1,7 +1,8 @@
-// An example Backbone application contributed by
-// [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses a simple
-// [LocalStorage adapter](backbone-localstorage.html)
-// to persist Backbone models within your browser.
+// Toronto is a messaging app written in backbone, node.js, and dnode
+
+
+/* [LocalStorage adapter](backbone-localstorage.html)
+   to persist Backbone models within your browser. */
 
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
